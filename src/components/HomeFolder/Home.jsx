a3461cfc218a4c48e0e96e88f5b6b2866795d40f
Vinayak S <oreva.vinayak@gmail.com>
2026-04-15 19:50:39 +0530

    feat: update Home component layout and improve loading state handling

--- src/components/HomeFolder/Home.jsx
+++ src/components/HomeFolder/Home.jsx
@@ -19,37 +19,38 @@ function Home() {
         setLoading(false);
       }
     };
-  });
+    getNotes();
+  }, []);
 
   return (
     <div className="w-screen h-screen bg-sky-50  relative pb-3000 flex justify-center">
       <HomeSearchDiv />
       <HomeNavBar />
-      <div className="h-100 w-300 mt-35 ml-20 bg-amber-300 flex flex-col">
+      <div className=" w-300 mt-35 ml-20 flex flex-col gap-10 p-10">
         {loading ? (
           <div className=" "> loading....</div>
         ) : notes.length === 0 ? (
-          <div className="h-60 w-full   bg-blue-300 flex flex-col ">
-            <div className="h-20 w-full bg-red-300 flex items-center justify-between px-20">
-              <p className="text-2xl font-bold">heading</p>
-              <div className="flex items-center">
-                <p className="p-5 text-2xl font-bold">00/00/0000</p>
-                <div className="h-13 w-25 rounded-2xl bg-black "></div>
-              </div>
-            </div>
-            <div className="flex-1 w-full  bg-green-300 p-10">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
-              maiores commodi cupiditate consectetur debitis molestias quidem
-              harum sed dolores. Deleniti inventore, nobis rem ea consequatur
-              autem sit quae quibusdam nemo Lorem ipsum dolor sit, amet
-              consectetur adipisicing elit. Quis quibusdam quidem voluptas
-              aliquam! Porro veniam corporis sapiente minus, magni labore
-              deserunt enim, ea voluptatum, dolore accusantium dignissimos
-              dolores. Repellendus, iste!Lorem
-            </div>
-          </div>
+          <p>backend is not sending data...</p>
         ) : (
-          <></>
+          notes.map((items) => {
+            return (
+              <div
+                className="h-60 w-full   bg-blue-300 flex flex-col "
+                key={items.noteId}
+              >
+                <div className="h-20 w-full  flex items-center justify-between ">
+                  <p className="text-2xl font-bold">{items.noteHeading}</p>
+                  <div className="flex items-center">
+                    <p className="p-5 text-2xl font-bold">
+                      {items.noteAddedDate}
+                    </p>
+                    <div className="h-13 w-25 rounded-2xl bg-black "></div>
+                  </div>
+                </div>
+                <div className="flex-1 w-full  p-10">{items.noteContent}</div>
+              </div>
+            );
+          })
         )}
       </div>
     </div>
