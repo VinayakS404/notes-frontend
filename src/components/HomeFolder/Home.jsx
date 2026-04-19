1b7ff72885de86e4c6e7771e533c6a86f2eba0a9
Vinayak S <oreva.vinayak@gmail.com>
2026-04-19 20:06:58 +0530

    feat: add new icons, update Home component layout, and enhance search functionality

--- src/components/HomeFolder/Home.jsx
+++ src/components/HomeFolder/Home.jsx
@@ -2,10 +2,18 @@ import { useEffect, useState } from "react";
 import HomeNavBar from "./HomeNaveBar.jsx";
 import HomeSearchDiv from "./HomeSearchDiv.jsx";
 import axios from "axios";
+import downIcon from "../../../assets/down.png";
 
 function Home() {
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const bgColors = {
+  "bg-red-100": "bg-red-100",
+  "bg-blue-100": "bg-blue-100",
+  "bg-green-100": "bg-green-100",
+};
+
 
   useEffect(() => {
     const getNotes = async () => {
@@ -21,16 +29,53 @@ function Home() {
     };
     getNotes();
   }, []);
+  
 
   return (
     <div className="w-screen h-screen bg-sky-50  relative pb-3000 ">
       <div className="  flex justify-center">
         <HomeSearchDiv />
         <HomeNavBar />
-        <div className="flex flex-col bg-red-100 w-300 mt-35 ml-20 rounded-xl">
-          <div className="bg-green-200">
-            <p className="text-2xl">The Note</p>
-            <div><button>all</button> <button></button>Favorate  <button>pin</button> </div>
+        <div className="flex flex-col w-300 mt-35 ml-20 rounded-xl">
+          <div className="px-10 flex flex-col gap-3">
+            <div className="flex  items-center justify-between ">
+              <div className="flex gap-2 items-center">
+                <img
+                  src={downIcon}
+                  alt="back arrow"
+                  className="h-8 rotate-90 mt-"
+                />
+                <p className="text-2xl ">
+                  <span className="text-[#7C7C7C]">Home</span> / All notes
+                </p>
+              </div>
+              <div className="flex ">
+                <img
+                  src={downIcon}
+                  alt="back arrow"
+                  className="h-8 rotate-90 mt-"
+                />
+                <img
+                  src={downIcon}
+                  alt="back arrow"
+                  className="h-8 rotate-90 mt-"
+                />
+              </div>
+            </div>
+            <div>
+              <div className=" flex gap-2 ">
+                <button className="bg-gray-100 border-2 border-gray-300 rounded-sm px-4 flex items-center justify-center">
+                  All
+                </button>
+                <button className="bg-gray-100 border-2 border-gray-300 rounded-sm px-4">
+                  Favorate
+                </button>
+                <button className="bg-gray-100 border-2 border-gray-300 rounded-sm px-4">
+                  Pined
+                </button>
+              </div>
+              <div></div>
+            </div>
           </div>
           <div className="  grid grid-cols-2 gap-10 p-10 ">
             {loading ? (
@@ -39,9 +84,11 @@ function Home() {
               <p>backend is not sending data...</p>
             ) : (
               notes.map((items) => {
+                const dynamicColor = bgColors[items.colortag] || "bg-gray-100";
+                console.log(items.colortag)
                 return (
                   <div
-                    className="h-60 w-full   bg-sky-50 flex flex-col p-10 rounded-2xl rounded-b-sm border-b-5 border-black shadow-sm"
+                    className={` h-60 w-full ${dynamicColor} flex flex-col p-10 rounded-2xl shadow-sm`}
                     key={items.noteId}
                   >
                     <div className="h-20 w-full  flex items-center justify-between ">
--- src/components/HomeFolder/HomeSearchDiv.jsx
+++ src/components/HomeFolder/HomeSearchDiv.jsx
@@ -1,9 +1,9 @@
 import searchIcon from "../../../assets/searchIcon.png";
 import downIcon from "../../../assets/down.png";
-import removeIcon from "../../../assets/remove.png";
+import removeGrayIcon from "../../../assets/remove-gray.png";
 function HomeSearchDiv() {
   return (
-    <div className=" w-full h-15   fixed top-10 flex justify-center pl-20">
+    <div className=" w-full h-30  fixed left-0 flex justify-start  bg-sky-50 shadow-sm">
       <div className=" h-full w-[calc(100%-150px)] flex items-center justify-center gap-2">
         <div className="h-12 min-w-100 rounded-xl bg-white border-2 border-gray-300 gap-3 pl-3 flex items-center justify-start">
           <img src={searchIcon} alt="search icon" className="h-5 " />
@@ -21,9 +21,9 @@ function HomeSearchDiv() {
           <p className="text-[#2A2A2A]">Filter by tag</p>
           <img src={downIcon} alt="extent" className="h-5 mt-1" />
         </div>
-        <div className="h-12 rounded-xl bg-gray-200 border-2 border-gray-300 gap-3 px-3 flex items-center justify-center">
-          <img src={removeIcon} alt="extent" className="h-5 " />
-          <p className="text-[#2A2A2A]">Reset filter</p>
+        <div className="h-12 rounded-xl bg-gray-100 border-2 border-gray-300 gap-3 px-3 flex items-center justify-center">
+          <img src={removeGrayIcon} alt="extent" className="h-5 " />
+          <p className="text-[#7C7C7C]">Reset filter</p>
         </div>
       </div>
     </div>
