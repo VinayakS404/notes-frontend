5793b6cc0b12f27dad53c0da18dfb119d21fd4cc
Vinayak S <oreva.vinayak@gmail.com>
2026-04-29 21:55:12 +0530

    feat: add new icons and images for HomeNavBar and SideBar; update layout and styles for improved UI

--- src/components/HomeFolder/HomeNaveBar.jsx
+++ src/components/HomeFolder/HomeNaveBar.jsx
@@ -1,33 +1,35 @@
+import homeIcon from "../../../assets/home_500dp_E3E3E3_FILL0_wght200_GRAD200_opsz48.png";
+import gearIcon from "../../../assets/settings_500dp_E3E3E3_FILL0_wght200_GRAD200_opsz48.png";
 import userIcon from "../../../assets/user.png";
+import darkModeIcon from "../../../assets/dark_mode_500dp_E3E3E3_FILL0_wght200_GRAD200_opsz48.png";
+import toolBarIcon from "../../../assets/toolbar_500dp_E3E3E3_FILL0_wght200_GRAD200_opsz48.png";
+
 const HomeNavBar = () => {
   return (
     <nav className=" h-screen w-10 py-1 fixed left-1 text-white z-10">
       <div className="flex flex-col items-center justify-between bg-[#2A2A2A] h-full w-18 py-10 rounded-xl">
-        <div className="flex flex-col items-center gap-2">
-          <div className="pb-5 ">
-            <a href="/">MyApp</a>
+        <div className=" h-full flex flex-col items-center justify-between gap-4">
+          <div className=" ">
+            <a href="/">App</a>
           </div>
 
-          <img src={userIcon} alt="userIcon" className="h-10" />
-    
-       
+          <div className="flex flex-col gap-3">
+            <div className="rounded hover:bg-[#404040] p-1 transition-colors duration-200">
+              <img src={homeIcon} alt="home icon" className="h-9" />
+            </div>
+            <div className="rounded hover:bg-[#404040] p-1 transition-colors duration-200">
+              <img src={toolBarIcon} alt="userIcon" className="h-9" />
+            </div>
+            <div className="rounded hover:bg-[#404040] p-1 transition-colors duration-200">
+              <img src={darkModeIcon} alt="userIcon" className="h-9" />
+            </div>
+            <div className="rounded hover:bg-[#404040] p-1 transition-colors duration-200">
+              <img src={gearIcon} alt="userIcon" className="h-9" />
+            </div>
+          </div>
 
-          <div className="flex flex-col  gap-4 text-[#2A2A2A]">
-            <div className="h-10 w-15 rounded-xl flex items-center justify-center bg-white">
-              trash
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <div className="flex flex-col  gap-4 text-[#2A2A2A]">
-            <div className="h-10 w-15 rounded-xl flex items-center justify-center bg-white">
-              setting
-            </div>
-          </div>
-          <div className="flex flex-col  gap-4 text-[#2A2A2A]">
-            <div className="h-13 w-13 rounded-full flex items-center justify-center bg-white">
-              profile
-            </div>
+          <div className="h-10 w-10 pr-0.5 rounded-full flex items-center justify-center bg-green-500">
+            <img src={userIcon} alt="user image" className="h-5 pl-1.5 " />
           </div>
         </div>
       </div>
--- src/components/HomeFolder/SideBar.jsx
+++ src/components/HomeFolder/SideBar.jsx
@@ -1,6 +1,49 @@
+import placeHolder from "../../../assets/apps_500dp_2A2A2A_FILL0_wght200_GRAD200_opsz48.png";
 function SideBar() {
   return (
-    <div className=" h-screen w-80  fixed left-1 top-1 rounded-2xl bg-sky-50 text-white border border-gray-300 z-9"></div>
+    <div className=" h-screen w-80 flex flex-col gap-2 pt-8 fixed left-1 top-1 pl-20 rounded-2xl bg-sky-50 text-white border border-gray-300 z-9">
+      <p className="text-gray-500 pl-2.5">this is a heading</p>
+      <div className="flex items-center gap-1 pl-2 hover:bg-[#d7dee5] h-10 mr-2  rounded text-[#2A2A2A] font-medium">
+        <img src={placeHolder} className="h-5 " />
+        <p>Apple</p>
+      </div>
+
+      <div className="flex items-center gap-1 pl-2 hover:bg-[#d7dee5] h-10 mr-2  rounded text-[#2A2A2A] font-medium">
+        <img src={placeHolder} className="h-5 " />
+        <p>Apple</p>
+      </div>
+
+      <div className="flex items-center gap-1 pl-2 hover:bg-[#d7dee5] h-10 mr-2  rounded text-[#2A2A2A] font-medium">
+        <img src={placeHolder} className="h-5 " />
+        <p>Apple</p>
+      </div>
+
+      <div className="flex items-center gap-1 pl-2 hover:bg-[#d7dee5] h-10 mr-2  rounded text-[#2A2A2A] font-medium">
+        <img src={placeHolder} className="h-5 " />
+        <p>Apple</p>
+      </div>
+      <p className="text-gray-500 pl-2.5">this is a heading</p>
+      <div className="flex items-center gap-1 pl-2 hover:bg-[#d7dee5] h-10 mr-2 rounded text-[#2A2A2A] font-medium">
+        <img src={placeHolder} className="h-5 " />
+        <p>Apple</p>
+      </div>
+
+      <div className="flex items-center gap-1 pl-2 hover:bg-[#d7dee5] h-10 mr-2  rounded text-[#2A2A2A] font-medium">
+        <img src={placeHolder} className="h-5 " />
+        <p>Apple</p>
+      </div>
+      
+
+      <div className="flex items-center gap-1 pl-2 hover:bg-[#d7dee5] h-10 mr-2  rounded text-[#2A2A2A] font-medium">
+        <img src={placeHolder} className="h-5 " />
+        <p>Apple</p>
+      </div>
+<p className="text-gray-500 pl-2.5">this is a heading</p>
+      <div className="flex items-center gap-1 pl-2 hover:bg-[#d7dee5] h-10 mr-2  rounded text-[#2A2A2A] font-medium">
+        <img src={placeHolder} className="h-5 " />
+        <p>Apple</p>
+      </div>
+    </div>
   );
 }
 export default SideBar;
