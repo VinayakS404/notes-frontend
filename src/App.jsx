31eb86e235ff6465e5975d0b35375f6bbc45d013
Vinayak S <oreva.vinayak@gmail.com>
2026-05-02 01:41:02 +0530

    feat: add AddNewNote component and update routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router";
 import Home from "./components/HomeFolder/Home.jsx";
+import AddNewNote from "./components/AddNewNote.jsx"
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/edit" element={<AddNewNote />}></Route>
     </Routes>
   );
 }
---
+++ src/components/AddNewNote.jsx
@@ -0,0 +1,4 @@
+function AddNewNote() {
+  return <div>this is a div</div>;
+}
+export default AddNewNote;
