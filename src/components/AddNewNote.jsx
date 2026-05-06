4c32dec169f7a43452db3325f6e666c8df6ab706
Vinayak S <oreva.vinayak@gmail.com>
2026-05-06 11:18:08 +0530

    feat: implement AddNewNote component with input fields for heading, content, and tags

--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -1,4 +1,17 @@
 function AddNewNote() {
-  return <div>this is a div</div>;
+  return (
+    <div className="flex items-center justify-center gap-10 h-screen">
+      <div className="">
+        <p>heading</p>
+        <input type="text" placeholder="enter something" className="border" />
+      </div>      <div className="">
+        <p>content</p>
+        <input type="text" placeholder="enter something" className="border" />
+      </div>      <div className="">
+        <p>tags</p>
+        <input type="text" placeholder="enter something" className="border" />
+      </div>
+    </div>
+  );
 }
 export default AddNewNote;
