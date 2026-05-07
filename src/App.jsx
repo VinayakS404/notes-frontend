5c8c18b7e9abe814652cef06699f3b472db6ed86
Vinayak S <oreva.vinayak@gmail.com>
2026-05-08 00:46:29 +0530

    fix: update route path for AddNewNote component and adjust layout styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/edit" element={<AddNewNote />}></Route>
+      <Route path="/add" element={<AddNewNote />}></Route>
     </Routes>
   );
 }
--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -1,16 +1,21 @@
 function AddNewNote() {
   return (
-    <div className="flex items-center justify-center gap-10 h-screen">
-      <div className="">
-        <p>heading</p>
-        <input type="text" placeholder="enter something" className="border" />
-      </div>      <div className="">
-        <p>content</p>
-        <input type="text" placeholder="enter something" className="border" />
-      </div>      <div className="">
-        <p>tags</p>
-        <input type="text" placeholder="enter something" className="border" />
-      </div>
+    <div className="flex flex-col items-center justify-center gap-8 h-screen">
+      <div className="flex items-center justify-center gap-10 ">
+        <div className="">
+          <p>heading</p>
+          <input type="text" placeholder="enter something" className="border" />
+        </div>{" "}
+        <div className="">
+          <p>content</p>
+          <input type="text" placeholder="enter something" className="border" />
+        </div>{" "}
+        <div className="">
+          <p>tags</p>
+          <input type="text" placeholder="enter something" className="border" />
+        </div>
+      </div> 
+      <button className="bg-black text-white font-medium active:bg-black/80 px-8 py-2 rounded-2xl">Upload</button>
     </div>
   );
 }
