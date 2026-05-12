ac6fe8b1a52353853295d061fc1a01fd0ca5234b
Vinayak S <oreva.vinayak@gmail.com>
2026-05-12 22:57:54 +0530

    fix: add missing commas in color definitions in Home component

--- src/components/HomeFolder/Home.jsx
+++ src/components/HomeFolder/Home.jsx
@@ -23,27 +23,27 @@ function Home() {
     blue: {
       bg: "bg-blue-100",
       text: "text-blue-700",
-      border: "border-blue-300"
+      border: "border-blue-300",
     },
     green: {
       bg: "bg-green-100",
       text: "text-green-700",
-      border: "border-green-300"
+      border: "border-green-300",
     },
     pink: {
       bg: "bg-pink-100",
       text: "text-pink-700",
-      border: "border-pink-300"
+      border: "border-pink-300",
     },
     yellow: {
       bg: "bg-yellow-100",
       text: "text-yellow-700",
-      border: "border-yellow-300"
+      border: "border-yellow-300",
     },
     white: {
       bg: "bg-gray-100",
       text: "text-gray-700",
-      border: "border-gray-300"
+      border: "border-gray-300",
     },
   };
   const getLightColor = () => {
